405430f21f743a6c2e8c0616492160404ab7fbbf
Elianmrey <elian060893@gmail.com>
2024-11-24 17:43:41 -0300

    Busca corrigida Funcionando como esperado

--- src/screens/SearchBar.jsx
+++ src/screens/SearchBar.jsx
@@ -1,60 +1,48 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import { TextInput, StyleSheet, View, FlatList, Text } from 'react-native';
-import { GetSearchResults } from '../services/SearchContent.module';
+import { GetSearchResults } from '../services/SearchContent';
 import Card from '../components/atomics/CardFilms';
 import CardTV from '../components/atomics/CardTV';
-import { ScrollView } from 'react-native';
-import { CheckBox } from 'react-native-elements';
+import { RadioButton } from 'react-native-paper';
 
 export default function SearchBar() {
     const [searchResult, setSearchResult] = useState([]);
-    const [movieIsChecked, setMovieIsChecked] = useState(false);
-    const [tvIsChecked, setTvIsChecked] = useState(false);
-    const [bothChecked, setBothChecked] = useState(false);
     const [searchType, setSearchType] = useState('movie or tv');
-
-
-    function onChek(value) {
-        if (value === 0) setSearchType('movie');
-        if (value === 1) setSearchType('tv');
-        if (value === 2) setSearchType('movie or tv');
-       
-        SearchContent(searchResult);
-    };
+    const [_, setQuery] = useState('');
 
     const SearchContent = async (value) => {
         if (!value.trim()) {
-            setSearchResult([]); 
+            setSearchResult([]);
             return;
         }
 
         try {
-            const results = await GetSearchResults(value); // Aguarda os resultados
-            setSearchResult(results); // Define os resultados no estado
+            const results = await GetSearchResults(value);
+            setSearchResult(results);
         } catch (error) {
             console.error('Erro ao buscar resultados:', error);
         }
     };
 
     const renderItem = ({ item }) => {
-        if (item.media_type === 'movie' && searchType === 'movie or tv') {
+        if (item.media_type === 'movie' && searchType === 'movie') {
             return <Card movie={item} addToFavorites={() => { }} />;
         }
-        if (item.media_type === 'tv' && searchType === 'movie or tv') {
+        if (item.media_type === 'tv' && searchType === 'tv') {
             return <CardTV tvProgram={item} addToFavorites={() => { }} />;
         }
-        if (searchType === 'movie') {
+        if (searchType === 'movie or tv') {
             if (item.media_type === 'movie') {
-                  return <Card movie={item} addToFavorites={() => { }} />;
-            }
-         } 
-        if (searchType === 'tv') {
-            if (item.media_type === 'tv') {
-                return <CardTV tvProgram={item} addToFavorites={() => { }} />;
-            }
+                return (
+                    <Card movie={item} addToFavorites={() => { }} />
+                )
+            } else if (item.media_type === 'tv') {
+                return (
+                    <CardTV tvProgram={item} addToFavorites={() => { }} />
+                )
+            };
         }
-
         return null;
     };
 
@@ -65,58 +53,50 @@ export default function SearchBar() {
         >
             <TextInput
                 style={styles.input}
-                placeholder="Pesquisar filmes..."
-                onChangeText={SearchContent}
+                placeholder="Pesquisar filmes ou programas de TV..."
+                onChangeText={(value) => {
+                    setQuery(value);
+                    SearchContent(value);
+                }}
             />
             <View style={styles.container}>
-                <View style={styles.filterContainer}>
-                    <Text style={styles.filterTitle}>Buscar por:</Text>
-                </View>
-
-                <View style={styles.checkboxContainer}>
-                    
+                <Text style={styles.filterTitle}>Buscar por:</Text>
+                <RadioButton.Group
+                    onValueChange={(newValue) => setSearchType(newValue)}
+                    value={searchType}>
                     <View style={styles.checkboxRow}>
-                        <CheckBox
-                            title={'Filmes'}
-                            onPress={() => { setMovieIsChecked(!movieIsChecked); onChek(0); }}
-                            containerStyle={styles.checkbox}
-                            checked={movieIsChecked}/>
+                        <RadioButton value="movie" />
+                        <Text style={styles.checkboxText}>Filmes</Text>
                     </View>
-
                     <View style={styles.checkboxRow}>
-                        <CheckBox
-                            title={'Programas de TV'}
-                            onValueChange={() => { setTvIsChecked(!tvIsChecked); onChek(1); }}
-                            containerStyle={styles.checkbox}
-                            checked={tvIsChecked}/>
+                        <RadioButton value="tv" />
+                        <Text style={styles.checkboxText}>Programas de TV</Text>
                     </View>
-
                     <View style={styles.checkboxRow}>
-                        <CheckBox
-                            title={'Ambos'}
-                            containerStyle={styles.checkbox}
-                            value={bothChecked}
-                            onValueChange={() => { setBothChecked(!bothChecked); onChek(3); }}
-                            checked={bothChecked}/>
+                        <RadioButton value="movie or tv" />
+                        <Text style={styles.checkboxText}>Ambos</Text>
                     </View>
-                </View>
+                </RadioButton.Group>
             </View>
 
-            <ScrollView style={styles.listContainer}>
-               
-                {searchResult.length > 0 ?
-                    <View style={styles.searchEvolver}>
+            {searchResult.length > 0 ? (
+                <View style={styles.searchEvolver}>
                     <Text style={styles.resultsTitle}>Resultados</Text>
                     <FlatList
-                    data={searchResult}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id.toString()}
-                            contentContainerStyle={styles.searchContainer} /></View>
-                    :
-                    <View style={styles.noResults}>
-                        <Text style={styles.noResultsText}>Vamos encontrar os melhores filmes e programas de TV!</Text>
-                    </View>}
-            </ScrollView>
+                        data={searchResult}
+                        renderItem={renderItem}
+                        keyExtractor={(item) => item.id.toString()}
+                        numColumns={2}
+                        contentContainerStyle={styles.searchContainer}
+                    />
+                </View>
+            ) : (
+                <View style={styles.noResults}>
+                    <Text style={styles.noResultsText}>
+                        Vamos encontrar os melhores filmes e programas de TV!
+                    </Text>
+                </View>
+            )}
         </LinearGradient>
     );
 }
@@ -124,115 +104,68 @@ export default function SearchBar() {
 const styles = StyleSheet.create({
     gradientContainer: {
         flex: 1,
-        alignItems: 'flex-start',
+        alignItems: 'center',
         justifyContent: 'flex-start',
-        padding: 5,
+        padding: 10,
         width: '100%',
     },
-
-    listContainer: {
-        flex: 1,
-        width: '100%',
-        padding: 8,
-       
-    },
-
     input: {
         width: '100%',
-        height: 60,
+        height: 50,
         fontSize: 16,
-        paddingLeft: 20,
+        paddingHorizontal: 20,
         backgroundColor: '#fff',
         borderRadius: 15,
-        marginTop: 40,
+        marginVertical: 20,
     },
     container: {
-        alignItems: 'center',
         width: '100%',
-        
+        marginBottom: 20,
     },
-    checkboxContainer: {
-       flexDirection: 'row',
-        alignItems: 'center',
-        width: '100%',
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        borderRadius: 10,
-        justifyContent: 'space-evenly',
-        padding: 10,
-    },
-
-    checkboxRow: {
-     flexDirection: 'row',
-        alignItems: 'center',
-    },
-    checkbox: {
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-        borderWidth: 0,
-        borderColor: 'none',
-      
-    },
-
-    checkboxTitle: {
-       fontSize: 16,
-        color: '#fff',
+    filterTitle: {
+        fontSize: 18,
         fontWeight: 'bold',
-        marginLeft: -15,
+        color: '#fff',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    checkboxRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    checkboxText: {
+        fontSize: 16,
+        color: '#fff',
+        marginLeft: 10,
     },
     searchEvolver: {
-        width: '100%',
         flex: 1,
-        padding: 5,
-       },
+        width: '100%',
+        padding: 10,
+    },
     resultsTitle: {
         fontSize: 24,
         fontWeight: 'bold',
         color: '#fff',
         marginBottom: 10,
         textAlign: 'center',
-        margin: 20,
     },
     noResults: {
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 20,
         backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        padding: 10,
-        width: '100%',
-        marginBottom: 10,
         borderRadius: 10,
-
-       
     },
     noResultsText: {
         fontSize: 16,
         fontWeight: 'bold',
-        color: '#fff', 
+        color: '#fff',
     },
     searchContainer: {
-        width: '100%',
-        flex: 1,
-        padding: 5,
         flexDirection: 'row',
-        gap: 15,
         flexWrap: 'wrap',
-        justifyContent: 'space-evenly',
-     
-       
+        justifyContent: 'space-between',
     },
-    
-    filterContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-      
-      },
-    filterTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-       
-        color: '#fff',
-        margin: 10,
-    },
-   
 });
